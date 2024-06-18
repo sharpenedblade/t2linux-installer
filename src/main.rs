c53ee08b23aa48831511069a50676da184868879
sharpenedblade <sharpenedblade@proton.me>
2024-06-18 13:30:43 -0700

    Initial GUI

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,87 @@
-use iced::{widget::text, Sandbox, Settings};
+use iced::{
+    widget::{button, checkbox, column, radio, text},
+    Sandbox, Settings,
+};
 
-struct App {}
+struct App {
+    distro: Option<Distro>,
+    auto_partitioning: bool,
+    target_disk: Option<String>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Copy)]
+enum Distro {
+    Arch,
+    Fedora,
+    Ubuntu,
+}
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-enum AppMessage {}
+enum AppMessage {
+    PickDistro(Distro),
+    ToggleAutoPartitioning(bool),
+    PickDisk(String),
+}
 
 impl Sandbox for App {
     type Message = AppMessage;
 
     fn new() -> Self {
-        Self {}
+        Self {
+            distro: None,
+            auto_partitioning: true,
+            target_disk: None,
+        }
     }
 
     fn title(&self) -> String {
         String::from("t2linux Installer")
     }
 
-    fn update(&mut self, _message: Self::Message) {}
+    fn update(&mut self, _message: Self::Message) {
+        match _message {
+            AppMessage::PickDistro(distro) => self.distro = Some(distro),
+            AppMessage::ToggleAutoPartitioning(b) => self.auto_partitioning = b,
+            AppMessage::PickDisk(s) => self.target_disk = Some(s),
+        }
+    }
 
     fn view(&self) -> iced::Element<Self::Message> {
-        text("Hello").into()
+        let mut disk_list = column![text("Choose instalation disk").size(24)].spacing(4);
+        for disk in ["disk0", "disk2"] {
+            disk_list = disk_list.push(radio(
+                disk,
+                &disk.to_string(),
+                self.target_disk.as_ref(),
+                |s| AppMessage::PickDisk(s.into()),
+            ));
+        }
+        column![
+            column![
+                text("Choose a distro").size(24),
+                radio("Arch", Distro::Arch, self.distro, AppMessage::PickDistro),
+                radio(
+                    "Fedora",
+                    Distro::Fedora,
+                    self.distro,
+                    AppMessage::PickDistro
+                ),
+                radio(
+                    "Ubuntu",
+                    Distro::Ubuntu,
+                    self.distro,
+                    AppMessage::PickDistro
+                )
+            ]
+            .spacing(4),
+            disk_list,
+            checkbox("Automatic partitioning", self.auto_partitioning)
+                .on_toggle(AppMessage::ToggleAutoPartitioning),
+            button("Begin installation")
+        ]
+        .spacing(16)
+        .padding(8)
+        .into()
     }
 }
 
