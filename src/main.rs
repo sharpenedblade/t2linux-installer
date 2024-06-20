6ed06cf80c4cb76919837f866454662f6c3c800c
sharpenedblade <sharpenedblade@proton.me>
2024-06-19 19:17:26 -0700

    Add support for compressed ISOs
    This currently only supports zip archive. Additionally, this will assume
    the first file in the zip is the iso.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,13 @@ mod ui {
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
 struct Distro {
     name: String,
+    iso_compression: Option<CompressionAlgorithim>,
     iso: Vec<String>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
+enum CompressionAlgorithim {
+    Zip,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
@@ -49,6 +55,15 @@ impl InstallSettings {
         for url in &self.distro.iso {
             let mut request: reqwest::blocking::Response = client.get(url).send().unwrap();
             request.copy_to(&mut iso_file).unwrap();
+        }
+        if let Some(compression_algo) = &self.distro.iso_compression {
+            match compression_algo {
+                CompressionAlgorithim::Zip => {
+                    let mut archive = zip::ZipArchive::new(iso_file.try_clone().unwrap()).unwrap();
+                    let mut decompressed_file = archive.by_index(0).unwrap();
+                    std::io::copy(&mut decompressed_file, &mut iso_file).unwrap();
+                }
+            }
         }
         fs::OpenOptions::new()
             .read(true)
