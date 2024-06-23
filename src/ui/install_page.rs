4461185f4e272c66d50751189823ed3f7b6f914c
sharpenedblade <sharpenedblade@proton.me>
2024-06-22 19:26:03 -0700

    Show progress in install_page

--- src/ui/install_page.rs
+++ src/ui/install_page.rs
@@ -1,10 +1,19 @@
 use crate::ui::app::{AppMessage, Page};
 use crate::InstallSettings;
-use iced::widget::{button, column};
+use iced::widget::{button, column, container, row, text};
+use iced::Length;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct InstallPage {
     install_settings: InstallSettings,
+    state: InstallState,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum InstallState {
+    NotStarted,
+    DownloadingIso,
+    Done,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -15,7 +24,10 @@ pub enum InstallPageMessage {
 
 impl InstallPage {
     pub fn new(install_settings: InstallSettings) -> Self {
-        Self { install_settings }
+        Self {
+            install_settings,
+            state: InstallState::NotStarted,
+        }
     }
 }
 
@@ -28,25 +40,45 @@ impl Page for InstallPage {
         if let AppMessage::InstallPage(msg) = message {
             match msg {
                 InstallPageMessage::StartInstallation => {
+                    self.state = InstallState::DownloadingIso;
                     let s: Self = self.clone();
                     command = iced::Command::perform(
                         async move {
-                            s.install_settings.install().await;
+                            s.install_settings.install().await.unwrap();
                         },
                         |_| AppMessage::InstallPage(InstallPageMessage::IsoDownloadEnd),
                     )
                 }
-                InstallPageMessage::IsoDownloadEnd => {}
+                InstallPageMessage::IsoDownloadEnd => self.state = InstallState::Done,
             }
         }
         (None, command)
     }
     fn view(&self) -> iced::Element<AppMessage> {
-        column![
-            button("Start Installation").on_press(AppMessage::InstallPage(
-                InstallPageMessage::StartInstallation
-            ))
-        ]
+        container(match self.state {
+            InstallState::NotStarted => {
+                column![
+                    text("Start Installation").size(24),
+                    text("This will permanently and irreversably modify your system. Make sure you have selected the right disk for the ISO. You will not be able to recover your data after clicking start."),
+                    button("Start Installation").on_press(AppMessage::InstallPage(
+                        InstallPageMessage::StartInstallation
+                    ))
+                ].spacing(16).width(600)
+            }
+            InstallState::DownloadingIso => {
+                column![text("Downloading ISO").size(24), text("Please wait...")].spacing(16)
+            }
+            InstallState::Done => {
+                column![
+                    text("Ready for installation!").size(24),
+                    text("You can reboot to the linux installer now.")
+                ].spacing(16)
+            }
+        })
+        .align_x(iced::alignment::Horizontal::Center)
+        .align_y(iced::alignment::Vertical::Center)
+        .width(Length::Fill)
+        .height(Length::Fill)
         .into()
     }
 }
