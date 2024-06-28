c1812a1eca7e21224001e14dade8931f2abdb50a
sharpenedblade <sharpenedblade@proton.me>
2024-06-27 22:24:22 -0700

    Add code to flash ISOs to disk

--- src/install.rs
+++ src/install.rs
@@ -2,6 +2,8 @@ use crate::distro::Distro;
 use crate::error::Error;
 use anyhow::Result;
 use futures::{Stream, StreamExt};
+use std::fs;
+use std::io;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum InstallStep {
@@ -23,6 +25,7 @@ pub enum InstallProgress {
 struct Installer {
     step: InstallStep,
     settings: InstallSettings,
+    iso_file: Option<fs::File>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -35,7 +38,13 @@ impl InstallSettings {
     pub fn new(distro: Distro, flash_disk: String) -> Self {
         Self { distro, flash_disk }
     }
-    async fn flash_iso(&self) -> Result<()> {
+    async fn flash_iso(&self, iso_file: &mut fs::File) -> Result<()> {
+        let mut target_disk = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(format!("/dev/{}", &self.flash_disk))?;
+        io::copy(iso_file, &mut target_disk)?;
         Ok(())
     }
     pub fn install(&self) -> impl Stream<Item = InstallProgress> {
@@ -44,6 +53,7 @@ impl InstallSettings {
             Installer {
                 step: InstallStep::Start,
                 settings,
+                iso_file: None,
             },
             |mut state| async move {
                 match state.step {
@@ -52,15 +62,20 @@ impl InstallSettings {
                         Some((InstallProgress::Started, state))
                     }
                     InstallStep::DownloadIso => {
-                        let Ok(_iso) = state.settings.distro.download_iso().await else {
+                        let Ok(iso) = state.settings.distro.download_iso().await else {
                             state.step = InstallStep::Finished;
                             return Some((InstallProgress::Failed(Error::IsoDownload), state));
                         };
+                        state.iso_file = Some(iso);
                         state.step = InstallStep::FlashIso;
                         Some((InstallProgress::DownloadedIso, state))
                     }
                     InstallStep::FlashIso => {
-                        let Ok(_) = state.settings.flash_iso().await else {
+                        let Ok(_) = state
+                            .settings
+                            .flash_iso(state.iso_file.as_mut().unwrap())
+                            .await
+                        else {
                             state.step = InstallStep::Finished;
                             return Some((InstallProgress::Failed(Error::IsoFlash), state));
                         };
