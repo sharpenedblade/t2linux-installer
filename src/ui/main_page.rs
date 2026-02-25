f96e8221d8e3639e8d9b125adb455be962f716c4
sharpenedblade <sharpenedblade@proton.me>
2026-02-24 16:14:42 -0800

    frontend for block device flashing

--- src/ui/main_page.rs
+++ src/ui/main_page.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{path::PathBuf, str::FromStr, sync::Arc};
 
 use crate::ui::{
     app::{AppMessage, Page},
@@ -6,7 +6,7 @@ use crate::ui::{
 };
 use crate::{distro::Distro, install::InstallSettings};
 use iced::{
-    Length, Task,
+    Color, Length, Task,
     widget::{button, column, container, radio, scrollable, text},
 };
 
@@ -15,26 +15,44 @@ use super::finish_page::FinishPage;
 #[derive(Debug, Clone)]
 pub enum MainPageMessage {
     LoadDistroList(Vec<Distro>),
+    LoadBlockDeviceList(Vec<String>),
     Err(Arc<anyhow::Error>),
     PickDistro(usize),
+    OpenTargetPicker,
     TriggerFilePicker,
     PickIsoFile(PathBuf),
+    PickBlockDeviceIndex(usize),
     StartInstall,
     Ignore,
 }
 
+enum MainPageState {
+    Distro,
+    Target,
+}
+
+#[derive(Clone)]
+enum DownloadTarget {
+    BlockDev(usize),
+    File(PathBuf),
+}
+
 pub struct MainPage {
+    state: MainPageState,
     distro_list: Option<Vec<Distro>>,
+    block_dev_list: Option<Vec<String>>,
     distro_index: Option<usize>,
-    iso_file: Option<PathBuf>,
+    download_target: Option<DownloadTarget>,
 }
 
 impl MainPage {
     pub fn new() -> Self {
         Self {
+            state: MainPageState::Distro,
             distro_list: None,
+            block_dev_list: None,
             distro_index: None,
-            iso_file: None,
+            download_target: None,
         }
     }
 }
@@ -48,7 +66,7 @@ impl Page for MainPage {
                 MainPageMessage::PickDistro(distro_index) => self.distro_index = Some(distro_index),
                 MainPageMessage::StartInstall => {
                     if let Some(distro_index) = self.distro_index
-                        && let Some(iso_file) = self.iso_file.clone()
+                        && let Some(DownloadTarget::File(iso_file)) = self.download_target.clone()
                         && let Some(distro_list) = self.distro_list.clone()
                     {
                         let install_settings = InstallSettings::new(
@@ -72,7 +90,12 @@ impl Page for MainPage {
                         "unknown.iso".to_owned()
                     });
                 }
-                MainPageMessage::PickIsoFile(path_buf) => self.iso_file = Some(path_buf),
+                MainPageMessage::PickIsoFile(path_buf) => {
+                    self.download_target = Some(DownloadTarget::File(path_buf))
+                }
+                MainPageMessage::PickBlockDeviceIndex(i) => {
+                    self.download_target = Some(DownloadTarget::BlockDev(i))
+                }
                 MainPageMessage::Ignore => {}
                 MainPageMessage::LoadDistroList(distros) => self.distro_list = Some(distros),
                 MainPageMessage::Err(error) => {
@@ -81,6 +104,10 @@ impl Page for MainPage {
                         super::finish_page::FinishState::Error(error),
                     )))
                 }
+                MainPageMessage::OpenTargetPicker => {
+                    self.state = MainPageState::Target;
+                }
+                MainPageMessage::LoadBlockDeviceList(items) => self.block_dev_list = Some(items),
             }
         }
         if self.distro_list.is_none() {
@@ -89,32 +116,16 @@ impl Page for MainPage {
         (page, task)
     }
     fn view(&self) -> iced::Element<AppMessage> {
-        let mut distro_list = column![].spacing(16);
-        if let Some(distros) = &self.distro_list {
-            for (i, distro) in distros.iter().enumerate() {
-                distro_list =
-                    distro_list.push(radio(distro.name.clone(), i, self.distro_index, |_| {
-                        AppMessage::Main(MainPageMessage::PickDistro(i))
-                    }));
-            }
-        }
-
-        container(
-            column![
-                text("Choose a distro").size(24),
-                scrollable(distro_list).height(400).width(400),
-                button("Chose file location")
-                    .on_press(AppMessage::Main(MainPageMessage::TriggerFilePicker)),
-                button("Next").on_press(AppMessage::Main(MainPageMessage::StartInstall))
-            ]
-            .spacing(16)
-            .padding(8),
-        )
-        .align_x(iced::alignment::Horizontal::Center)
-        .align_y(iced::alignment::Vertical::Center)
-        .width(Length::Fill)
-        .height(Length::Fill)
-        .into()
+        let e = match self.state {
+            MainPageState::Distro => self.distro_picker_view(),
+            MainPageState::Target => self.target_picker_view(),
+        };
+        container(e)
+            .align_x(iced::alignment::Horizontal::Center)
+            .align_y(iced::alignment::Vertical::Center)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 
     fn subscription(&self) -> iced::Subscription<AppMessage> {
@@ -124,7 +135,67 @@ impl Page for MainPage {
 
 impl MainPage {
     pub fn init_tasks() -> Task<AppMessage> {
-        get_distro_list()
+        Task::batch([get_distro_list(), get_block_dev_list()])
+    }
+    fn distro_picker_view(&self) -> iced::widget::Column<'_, AppMessage> {
+        let mut distro_list = column![].spacing(16);
+        if let Some(distros) = &self.distro_list {
+            for (i, distro) in distros.iter().enumerate() {
+                distro_list =
+                    distro_list.push(radio(distro.name.clone(), i, self.distro_index, |_| {
+                        AppMessage::Main(MainPageMessage::PickDistro(i))
+                    }));
+            }
+        }
+        column![
+            text("Choose a distro").size(24),
+            scrollable(distro_list).height(400).width(400),
+            button("Next").on_press(AppMessage::Main(MainPageMessage::OpenTargetPicker))
+        ]
+        .spacing(16)
+        .padding(8)
+    }
+    fn target_picker_view(&self) -> iced::widget::Column<'_, AppMessage> {
+        column![
+            self.file_path_view(),
+            self.block_dev_view(),
+            button("Begin Download").on_press_maybe(if self.download_target.is_some() {
+                Some(AppMessage::Main(MainPageMessage::StartInstall))
+            } else {
+                None
+            })
+        ]
+        .spacing(32)
+        .padding(8)
+    }
+    fn file_path_view(&self) -> iced::widget::Container<'_, AppMessage> {
+        let mut col = column![text("Download to a file").size(24),];
+        if let Some(DownloadTarget::File(path)) = &self.download_target {
+            col = col.push(text(format!("{}", path.display())));
+        } else {
+            col = col.push(text("No download path selected"))
+        }
+        col = col.push(
+            button("Choose file path ")
+                .on_press(AppMessage::Main(MainPageMessage::TriggerFilePicker)),
+        );
+        container(col.spacing(16)).width(350)
+    }
+    fn block_dev_view(&self) -> iced::widget::Container<'_, AppMessage> {
+        let selected_i = if let Some(DownloadTarget::BlockDev(n)) = self.download_target {
+            Some(n)
+        } else {
+            None
+        };
+        let mut list = column![].spacing(16);
+        if let Some(devs) = &self.block_dev_list {
+            for (cur_i, dev) in devs.iter().enumerate() {
+                list = list.push(radio(dev.clone(), cur_i, selected_i, |_| {
+                    AppMessage::Main(MainPageMessage::PickBlockDeviceIndex(cur_i))
+                }));
+            }
+        }
+        container(column![text("Flash to a disk").size(24), list,].spacing(16)).width(350)
     }
 }
 
@@ -145,6 +216,14 @@ fn open_file(name: String) -> Task<AppMessage> {
 fn get_distro_list() -> Task<AppMessage> {
     Task::future(Distro::get_all()).then(|handle| match handle {
         Ok(distros) => Task::done(AppMessage::Main(MainPageMessage::LoadDistroList(distros))),
+        Err(e) => Task::done(AppMessage::Main(MainPageMessage::Err(Arc::new(e)))),
+    })
+}
+
+fn get_block_dev_list() -> Task<AppMessage> {
+    Task::future(async { Ok(vec!["sda".to_owned(), "sdb".to_owned()]) }).then(|handle| match handle
+    {
+        Ok(list) => Task::done(AppMessage::Main(MainPageMessage::LoadBlockDeviceList(list))),
         Err(e) => Task::done(AppMessage::Main(MainPageMessage::Err(Arc::new(e)))),
     })
 }
